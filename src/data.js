c121a3626222e6b16464e48f2583af0348b6dbea
unknown <saks0705@gmail.com>
2023-09-27 17:58:49 +0530

    sc

---
+++ src/data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+    {
+      id: "abc",
+      name: "White tote bag",
+      price: 20,
+      desc: "Lorem ipsum dolor sit amet. Lorem, ipsum.",
+      img: "image/img1.jpg",
+    },
+    {
+      id: "mno",
+      name: "Office bag",
+      price: 120,
+      desc: "Lorem ipsum dolor sit amet. Lorem, ipsum.",
+      img: "image/img2.jpg",
+    },
+    {
+      id: "pqr",
+      name: "Black and bold",
+      price: 70,
+      desc: "Lorem ipsum dolor sit amet. Lorem, ipsum.",
+      img: "image/img3.jpg",
+    },
+    {
+      id: "xyz",
+      name: "Olive green Tote bag",
+      price: 100,
+      desc: "Lorem ipsum dolor sit amet. Lorem, ipsum.",
+      img: "image/img4.jpg",
+    },
+  ];
